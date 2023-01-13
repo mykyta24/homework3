ecf4b5b17f63e23dd6a6a11ea00e4a6f7cf67a8d
mykyta24 <eldobest2@gmail.com>
2023-01-12 20:30:41 -0500

    4

---
+++ src/main/java/homework/homework.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class homework {
+    public static void main(String[] args) {
+        for (int i = 6; i>=0; i--) {
+            for (int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
